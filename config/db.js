d1a4223ba6b6671afc0b0502dca7867e9d00ce38
ramsai924 <ramsai924@gmail.com>
2020-08-21 12:54:05 +0530

    capstone ejs v2

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
-//mongodb+srv://capstone:<password>@cluster0.azviz.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb+srv://capstoneejs:capstoneejs@cluster0.nhuzf.mongodb.net/capstone-ejs-app?retryWrites=true&w=majority
 //mongodb://localhost:27017/recycling
 const db = mongoose
-  .connect("mongodb://localhost:27017/capstoneBackend", {
+  .connect("mongodb+srv://capstoneejs:capstoneejs@cluster0.nhuzf.mongodb.net/capstone-ejs-app?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
